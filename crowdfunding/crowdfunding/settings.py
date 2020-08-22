36feaaac34bdd28d6c303ff6ecdfebf930939c61
varunpreet-code <cp0452472@gmail.com>
2020-08-22 12:45:21 +0800

    Delete for project working

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -46,7 +46,9 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ]
+
 }
 
 AUTH_USER_MODEL = 'users.CustomUser'
--- crowdfunding/project/views.py
+++ crowdfunding/project/views.py
@@ -71,6 +71,10 @@ class ProjectDetail(APIView):
         )
         if serializer.is_valid():
             serializer.save()
+    def delete(self, request, pk, format=None):
+        project = self.get_object(pk)
+        project.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class PledgeList(APIView):
     
@@ -93,9 +97,8 @@ class PledgeList(APIView):
                 status=status.HTTP_400_BAD_REQUEST
                 )
 
-    def delete_view(request, id):
+    
         
         
-        context ={} 
 
 # Create your views here.
