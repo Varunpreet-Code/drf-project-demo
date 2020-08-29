8e1146bc4cd800caed0969259c0d577296e3f766
varunpreet-code <cp0452472@gmail.com>
2020-08-30 00:08:22 +0800

    Update user

--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -33,4 +33,18 @@ class CustomUserDetail(APIView):
         user = self.get_object(pk)
         serializer = CustomUserSerializer(user)
         return Response(serializer.data)
+
+
+    def put(self, request, pk, format=None):
+        user = self.get_object(pk)
+        serializer = CustomUserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        user = self.get_object(pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 # Create your views here.
